6b230723eb9c1d00a89f4bedd52901fe27ad5b54
Mhd Risky Rianda Maulana <100455713+Riskyrianda@users.noreply.github.com>
2023-10-17 21:50:51 +0700

    Create array_2dimensi.cpp

---
+++ array_2dimensi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+
+{
+
+
+string Countries[2][4]={"Indonesia","Singapore","Malaysia","Thailand"},{"Germany","France","Spain","Belgium"}
+int k,b;
+
+cout<<k;
+
+}
+
+return 0;
+
+}
+
+
